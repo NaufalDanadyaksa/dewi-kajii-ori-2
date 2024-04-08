dce4048f0dd201e1ab922204b8823e61898138dd
Naufal Danadyaksa <64962349+NaufalDanadyaksa@users.noreply.github.com>
2024-04-08 15:26:47 +0700

    membuat detail page untuk berita dan sejarah (#7)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Atraksi;
+use App\Models\Berita;
 use App\Models\Homestay;
+use App\Models\Sejarah;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -22,5 +24,17 @@ class HomeController extends Controller
         $atraksi = Atraksi::with('images')->findOrFail($id);
 
         return view('atraksi.detail', compact('atraksi'));
+    }
+    public function showSejarah(string $id)
+    {
+        $sejarah = Sejarah::with('images')->findOrFail($id);
+
+        return view('sejarah.detail', compact('sejarah'));
+    }
+    public function showBerita(string $id)
+    {
+        $berita = Berita::with('images')->findOrFail($id);
+
+        return view('berita.detail', compact('berita'));
     }
 }
